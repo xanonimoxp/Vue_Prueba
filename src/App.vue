99d1722c07732e01cc58d213496123dde096a98d
unknown <xanonimoxp@hotmail.com>
2021-10-08 16:10:22 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <div id="header"></div>
+  <div id="maincontainer">
+    <h2>Todos</h2>
+    <Todos v-bind:todoslist="copyTodos" />
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+//import Search from './components/Serch.vue';
+import Todos from './components/Todo';
+//import TodoApp from './components/Todoitem.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos
+  },
+  data(){
+    return {
+      todos: [
+        {
+          id:0,
+          title:'comprar',
+          completed:false
+        },
+                {
+          id:3,
+          title:'chec',
+          completed:false
+        },
+                {
+          id:1,
+          title:'cena',
+          completed:true
+        },
+                {
+          id:0,
+          title:'xbox',
+          completed:false
+        }
+      ],
+      copyTodos: []
+    }
+  },
+  created(){
+    this.copyTodos=[...this.todos];
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ *{
+    box-sizing: border-box;
+  }
+  body{
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1.5em;
+    padding: 0;
+    margin: 0;
+  }
+  #main-container{
+    border: solid 1px #ccc;
+    width: 600px;
+    margin: 100px auto;
+  }
+  #header{
+    background: black;
+    padding: 10px;
+  }
+  h2{
+    padding: 0 10px;
+  }
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in Todoslist">
+            <div>{{todo.title}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Todos',
+    props:['todos'],
+}
+</script>
